1f4cb32f01b5198d912dc22d7e0c21e683c680ab
Lucas Lourenço <dev.lucaslourenco@gmail.com>
2023-07-10 11:50:01 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,17 +1,32 @@
+import pandas as pd
 import unicodedata
 
 class RecofUnicode():
+    flag = False
     
     def __new__(self, arg):
-        self.__adaptado = list()
-        return self.run(arg)
+        self.texto_tratado: str = ''
+        self.lista_tratada: list = list()
+        
+        if not isinstance(arg, list):    
+            self.get(self, arg)
+            return self.texto_tratado
+        
+        else:
+            RecofUnicode.flag = True
+            self.get(self, arg)
+            return self.lista_tratada
+        
 
-    def run(objeto):
-        txt_adaptado = ''.join(x for x in unicodedata.normalize('NFD', objeto) if unicodedata.category(x) != 'Mn')
-        txt_adaptado = txt_adaptado.replace('ç', 'c')
-        return txt_adaptado
+    def get(self, objeto):
+        recof = lambda item: ''.join(x for x in unicodedata.normalize('NFD', item) if unicodedata.category(x) != 'Mn')
+        
+        if not RecofUnicode.flag:
+            self.texto_tratado = recof(objeto)
+        else:
+            self.lista_tratada = [recof(each) for each in objeto]
+            
 
-
+                    
 if __name__=="__main__":
-  texto = RecofUnicode('áãàñç')
-  print(texto)
+    print(RecofUnicode(['çáãÀâ','ãçâé']))
